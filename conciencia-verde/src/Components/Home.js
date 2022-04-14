89bcbb5bc84523b13dd12c8e1845c80d65bf1266
J. Diego Fernández <JDFBdeveloper@gmail.com>
2022-04-14 16:52:31 -0300

    Stats

--- conciencia-verde/src/Components/Home.js
+++ conciencia-verde/src/Components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import s from './Home.module.css';
 import logo from '../img/logo.png';
+import circle from '../img/circle.png';
+import CountUp from 'react-countup';
 
 export default function Home(){
+    
     return(
         <div className={s.container}>
             <div className={s.banner}>
@@ -16,10 +19,31 @@ export default function Home(){
                 </div>
             </div>
             <div className={s.dataContainer}>
+
                 <div className={s.stats}>
-                    <h3>Estadisticas</h3>
+                    <h3 className={s.statsTitle}>Estadisticas</h3>
+                    <div className={s.statsData}>
+                        <div className={s.bags}>
+                            <div className={s.circleBags} style={{backgroundImage: `url(${circle})`}}>
+                                <CountUp duration={4} useEasing={true} className={s.counter} end={69} />
+                            </div>
+                            <p className={s.statsLabel}>Bolsas de basura desechadas</p>
+                        </div>
+                        <div className={s.kg}>
+                            <div className={s.circleBags} style={{backgroundImage: `url(${circle})`}}>
+                                <CountUp duration={4} useEasing={true} className={s.counter} end={420} />
+                            </div>
+                            <p className={s.statsLabel}>Kilos de basura sacados del mar</p>
+                        </div>
+                    </div>
                 </div>
+
+                <div className={s.fachasContainer}>
+                    <h3 className={s.statsTitle}>Fachas</h3>
+                </div>
+
             </div>
+
         </div>
     )
 }

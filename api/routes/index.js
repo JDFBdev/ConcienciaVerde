39128b1c8d232e6e3be3dba1e4699ab8019905ea
santiset <sset@fi.uba.ar>
2022-04-19 18:12:49 -0300

    asd

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,10 +4,12 @@ var router = express.Router();
 const createUser = require('./createUser');
 const allUsers = require('./allUsers');
 const createDay = require('./createDay');
+const allDays = require('./allDays');
 
 router.use('/createUser', createUser);
 router.use('/allUsers', allUsers);
 router.use('/createDay', createDay);
+router.use('/allDays', allDays);
 
 /* GET home page. */
 router.get('/', (req, res) => {

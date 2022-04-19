ba4f14a61d7287683664caac7bd1aff6447b6e65
J. Diego Fernández <JDFBdeveloper@gmail.com>
2022-04-19 18:18:45 -0300

    Connecting front and back

--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 const {
-    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
+    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, DATABASE_URL
   } = process.env;
 
 module.exports = new Sequelize({
@@ -12,12 +12,13 @@ module.exports = new Sequelize({
     port: DB_PORT,
     dialect: "postgres",
     dialectOptions: {
+        connectionString: DATABASE_URL,
         ssl: {
             require: true,
             rejectUnauthorized: false
         }
-     },
+    },
     define: {
         timestamps: false
-    }
-  });
+    }   
+});
